d1629924579612dc1e5f026763bed9c940493817
{rnantes} <{reid.nantes@gmail.com}>
2019-11-27 02:13:56 -0500

    update to forked sql-kit package

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/sql-kit.git", .branch("master")),
+        .package(url: "https://github.com/rnantes/sql-kit.git", .branch("master")),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: ["NIO", "Logging"]),
