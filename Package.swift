4c2b2e340b27b86367688015e53a10e3499c4aea
{rnantes} <{reid.nantes@gmail.com}>
2019-12-18 15:43:46 -0500

    updated minimum iOS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "fluent-kit",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_14),
+       .iOS(.v11)
     ],
     products: [
         .library(name: "FluentKit", targets: ["FluentKit"]),
